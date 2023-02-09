55b3885b25a02815dcf818533475b1a44b593eb3
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-02-09 23:38:42 +0100

    feat: Create configuration class for integration tests

---
+++ src/test/java/com/example/statisticsservice/BaseIntegrationTestIT.java
@@ -0,0 +1,43 @@
+package com.example.statisticsservice;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+@DirtiesContext
+public abstract class BaseIntegrationTestIT {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Container
+    private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+            .withDatabaseName("statisticsdatabase")
+            .withUsername("root")
+            .withPassword("root")
+            .withReuse(true);
+
+    @DynamicPropertySource
+    private static void containerConfig(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", mySQLContainer::getDriverClassName);
+    }
+}
