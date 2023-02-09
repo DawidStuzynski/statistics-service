5f823d2f0254352ba29483aac09af43e8c0e9115
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-02-09 17:09:33 +0100

    feat: Create tests for StatisticService

---
+++ src/test/java/com/example/statisticsservice/service/StatisticServiceTest.java
@@ -0,0 +1,40 @@
+package com.example.statisticsservice.service;
+
+import com.example.statisticsservice.controller.dto.ResultDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class StatisticServiceTest {
+    @InjectMocks
+    private StatisticService statisticService;
+    @Mock
+    private MatchResultService matchResultService;
+    @Mock
+    private MatchStatisticsService matchStatisticsService;
+
+    @Test
+    void fetchStatistics_returnsCorrectStatistics() {
+        //given
+        ResultDto resultDto = new ResultDto("Barcelona", "Real Madrid", 2L, 1L);
+        String homeTeamStatistics = "Barcelona 1 3 2 1";
+        String awayTeamStatistics = "Real Madrid 1 0 1 2";
+
+        //when
+        when(matchStatisticsService.getTeamStatistics("Barcelona")).thenReturn(homeTeamStatistics);
+        when(matchStatisticsService.getTeamStatistics("Real Madrid")).thenReturn(awayTeamStatistics);
+
+        //then
+        String statistics = statisticService.fetchStatistics(resultDto);
+
+        assertEquals(homeTeamStatistics + "\n" + awayTeamStatistics, statistics);
+        verify(matchResultService).saveMathResults(resultDto);
+    }
+}
