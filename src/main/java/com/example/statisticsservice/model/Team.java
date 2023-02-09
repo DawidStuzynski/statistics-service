751e20a3bdb2f779195c42ffd6c68b2f0e69eb92
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-02-09 20:36:18 +0100

    feat: Add aliases for entity fields

--- src/main/java/com/example/statisticsservice/model/Team.java
+++ src/main/java/com/example/statisticsservice/model/Team.java
@@ -1,9 +1,11 @@
 package com.example.statisticsservice.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,19 +18,24 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "team")
 public class Team {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
+    @Column(name = "played_events")
     private Long playedEvents;
 
+    @Column(name = "gained_points")
     private Long gainedPoints;
 
+    @Column(name = "goals_scored")
     private Long goalsScored;
 
+    @Column(name = "goals_conceded")
     private Long goalsConceded;
 }
