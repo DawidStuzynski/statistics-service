d09e3ad49ba8d2675db87dea91909587d39a6ec5
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-02-12 20:53:59 +0100

    refactor: Add checking result in integration test

--- src/test/java/com/example/statisticsservice/StatisticServiceTestIT.java
+++ src/test/java/com/example/statisticsservice/StatisticServiceTestIT.java
@@ -4,13 +4,13 @@ import com.example.statisticsservice.controller.dto.MessageDto;
 import com.example.statisticsservice.controller.dto.MessageType;
 import com.example.statisticsservice.controller.dto.ResultDto;
 import com.example.statisticsservice.repository.TeamRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,14 +27,22 @@ class StatisticServiceTestIT extends BaseIntegrationTestIT {
 
     @Test
     void shouldReturnOkStatusWhenProperRequestIsMade() throws Exception {
+
+        //given
         ResultDto resultDto = new ResultDto("Barcelona", "Real Madrid", 2L, 1L);
         MessageDto messageDto = new MessageDto(MessageType.RESULT, resultDto);
 
-        ResultActions resultActions = mockMvc.perform(post("/statistics")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(messageDto)));
+        //when
+        MvcResult mvcResult = mockMvc.perform(post("/statistics")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(messageDto)))
+                .andExpect(status().isOk())
+                .andReturn();
 
-        resultActions.andExpect(status().isOk());
-        Assertions.assertEquals(2, teamRepository.findAll().size());
+        //then
+        String result = mvcResult.getResponse().getContentAsString();
+        String expectedResult = "Barcelona 1 3 2 1\nReal Madrid 1 0 1 2";
+        assertEquals(expectedResult, result);
+        assertEquals(2, teamRepository.findAll().size());
     }
 }
